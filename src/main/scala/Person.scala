e43b38457a49fb6bbc375c8efb7a2546461d00f3
dengsibao <dsibao@163.com>
2015-05-22 23:11:01 +0800

    add animal files

--- src/main/scala/Person.scala
+++ src/main/scala/Person.scala
@@ -2,4 +2,20 @@
 class Person {
   var name: String = _ 
   val age: Int = _ 
+
+  def this(name: String, age: Int) = {
+    this
+    this.name = name
+    this.age = age
+  }
+
+  override def toString = "name: " + name + "," + "age: " + age
+
+  override def hashCode = name.hashCode + age.hashCode
+}
+
+object Person {
+  def apply(name: String, age: Int) = new Person(name, age)
+
+  def run = "everyone can run"
 }
